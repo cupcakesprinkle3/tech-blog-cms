30f5f404a9203feee1ed8e2678ab9904a021fe1a
cupcakesprinkle3 <79061264+cupcakesprinkle3@users.noreply.github.com>
2021-07-17 07:50:17 -0700

    added and fixed sessions

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,16 +2,16 @@ const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
 
 // get all users
-router.get('/', (req, res) => {
-  User.findAll({
-    attributes: { exclude: ['password'] }
-  })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+// router.get('/', (req, res) => {
+//   User.findAll({
+//     attributes: { exclude: ['password'] }
+//   })
+//     .then(dbUserData => res.json(dbUserData))
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
 
 router.get('/:id', (req, res) => {
   User.findOne({
@@ -54,18 +54,47 @@ router.post('/', (req, res) => {
     email: req.body.email,
     password: req.body.password
   })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
+    })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
+// Post Users
+// router.post('/signup', (req, res) => {
+//   User.create({
+//     username: req.body.username,
+//     email: req.body.email,
+//     password: req.body.password
+//   })
+//     .then(dbUserData => {
+//       req.session.save(() => {
+//         req.session.user_id = dbUserData.id;
+//         req.session.email = dbUserData.email;
+//         req.session.loggedIn = true;
+
+//         res.json(dbUserData);
+//       });
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
 router.post('/login', (req, res) => {
-  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
     where: {
-      email: req.body.email
+      username: req.body.username
     }
   }).then(dbUserData => {
     if (!dbUserData) {
@@ -80,7 +109,14 @@ router.post('/login', (req, res) => {
       return;
     }
 
-    res.json({ user: dbUserData, message: 'You are now logged in!' });
+    req.session.save(() => {
+      // declare session variables
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
   });
 });
 
---
+++ controllers/home-routes.js
@@ -0,0 +1,56 @@
+const router = require('express').Router();
+
+// const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+  console.log(req.session);
+  Post.findAll({
+    include: [ User ]    
+    // attributes: [
+    //   'id',
+    //   'title',
+    //   'contents',
+    //   'created_at',
+    // ],
+    // include: [
+    //   {
+    //     model: Comment,
+    //     attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+    //     include: {
+    //       model: User,
+    //       attributes: ['username']
+    //     }
+    //   },
+    //   {
+    //     model: User,
+    //     // attributes: ['username']
+    //   }
+    // ]
+  })
+    .then(dbPostData => {
+      // pass a single post object into the homepage template
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('homepage', {
+        posts
+        // loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
+router.get('/login', (req, res) => {
+  console.log(req.session);
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
