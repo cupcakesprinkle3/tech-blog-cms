65f801760b6330c7355ec3255fd49d6762b17bc3
cupcakesprinkle3 <79061264+cupcakesprinkle3@users.noreply.github.com>
2021-07-19 19:32:37 -0700

    added some partials and helpers

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,6 +13,8 @@ const PORT = process.env.PORT || 3001;
 const session = require('express-session');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
 
 const sess = {
   secret: 'Super secret secret',
